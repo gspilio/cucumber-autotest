58959aeef33eb46d8888b17550e6fae324051cba
Ioannis Spiliopoulos <giannis.spiliopoulos@gmail.com>
2013-10-30 12:10:46 +0200

    Improve discover_spec

--- spec/autotest/discover_spec.rb
+++ spec/autotest/discover_spec.rb
@@ -1,42 +1,59 @@
 require "spec_helper"
 
-describe "autotest/discover.rb"
+describe "autotest/discover.rb" do
   
-  it "adds a discovery" do
-    expect(Autotest).to receive(:add_discovery)
-    load File.expand_path("../../../lib/autotest/discover.rb",__FILE__)
-  end
-
-  context "with AUTOFEATURE true" do
-    it 'adds "cucumber" to the list of discoveries' do
-      expect(Autotest).to receive(:add_discovery)
-      load File.expand_path("../../../lib/autotest/discover.rb",__FILE__)
-    end
-  end
-
-  context "with AUTOFEATURE false" do
-    it 'does not add "cucumber" to the list of discoveries' do
-      expect(Autotest).to not_receive(:add_discovery)
-      load File.expand_path("../../../lib/autotest/discover.rb",__FILE__)
-    end
-  end
-
-  context "with AUTOFEATURE undefined" do
-    before :all do
-      ENV.delete('AUTOFEATURE')
+  context "with features dir" do
+    before :each do
+      File.stub(:directory?).with("features") {true}
     end
 
-    it 'does not add "cucumber" to the list of discoveries' do
-      expect(Autotest).to not_receive(:add_discovery)
-      load File.expand_path("../../../lib/autotest/discover.rb",__FILE__)
-    end
+    context "with AUTOFEATURE true" do
+      before :all do
+        ENV['AUTOFEATURE'] = "true"
+      end
 
-    it 'puts help message' do
-      expect(IO).to receive(:puts).with(
-        %q{Not running features.
-           To run features in autotest, set AUTOFEATURE=true.
-           To remove this message, set AUTOFEATURE=false.}
-      )
-      load File.expand_path("../../../lib/autotest/discover.rb",__FILE__)
-    end  
+      it 'adds "cucumber" to the list of discoveries' do
+        expect(Autotest).to receive(:add_discovery)
+        load File.expand_path("../../../lib/autotest/discover.rb",__FILE__)
+      end
+    end
+  
+    context "with AUTOFEATURE false" do
+      before :all do
+        ENV['AUTOFEATURE'] = "false"
+      end
+
+      it 'does not add "cucumber" to the list of discoveries' do
+        expect(Autotest).not_to receive(:add_discovery)
+        load File.expand_path("../../../lib/autotest/discover.rb",__FILE__)
+      end
+    end
+  
+    context "with AUTOFEATURE undefined" do
+      before :all do
+        @orig_stdout = $stdout
+        $stdout = File.open('/dev/null', 'w')
+        ENV.delete('AUTOFEATURE')
+      end
+
+      after :all do
+        $stdout = @orig_stdout
+      end
+  
+      it 'does not add "cucumber" to the list of discoveries' do
+        expect(Autotest).not_to receive(:add_discovery)
+        load File.expand_path("../../../lib/autotest/discover.rb",__FILE__)
+      end
+  
+      it 'puts help message' do
+        expect($stdout).to receive(:puts).with(
+          %q{Not running features.
+To run features in autotest, set AUTOFEATURE=true.
+To remove this message, set AUTOFEATURE=false.}
+        )
+        load File.expand_path("../../../lib/autotest/discover.rb",__FILE__)
+      end  
+    end
   end
+end
+
