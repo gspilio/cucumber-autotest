112f2acaac4bf2315a96d871e40ab69390d3b2f2
Ioannis Spiliopoulos <giannis.spiliopoulos@gmail.com>
2013-10-30 12:11:09 +0200

    Refactor lib/autotest/discover.rb
    It calls Autotest.discovery when it should add "cucumber" in
    discoveries.

--- lib/autotest/discover.rb
+++ lib/autotest/discover.rb
@@ -1,11 +1,11 @@
-Autotest.add_discovery do
-  if File.directory?('features')
-    if ENV['AUTOFEATURE'] =~ /true/i
-      "cucumber"
-    elsif ENV['AUTOFEATURE'] =~ /false/i
-      # noop
-    else
-      puts "(Not running features.  To run features in autotest, set AUTOFEATURE=true.)"
-    end
+if File.directory?('features')
+  if ENV.has_key?('AUTOFEATURE')
+    Autotest.add_discovery {"cucumber"} if (ENV['AUTOFEATURE'] =~ /true/i)
+  else
+    help_message = 
+%q{Not running features.
+To run features in autotest, set AUTOFEATURE=true.
+To remove this message, set AUTOFEATURE=false.}
+    puts help_message
   end
 end
